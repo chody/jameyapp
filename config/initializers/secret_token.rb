d6232efcadd66f310ddbb26e2d8aa6c18259d897
Cody Hill <codyhill@bdogm.com>
2011-04-12 17:30:11 -0500

    jamey rules

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jameyapp::Application.config.secret_token = 'ead351c233d0919f637d071fca958b5d111935041990c2b63799ffa6fed856c648b6c6fcc5f42c167db4a24a0979e129c85c2593ed3cb744e290462451263937'
